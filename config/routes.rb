ba39ade2fb1880d85651d18a7b7242ed6f0c8943
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-07 12:48:24 +0630

    resourse&root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :feeds do
-      collection do
+     collection do
         post :confirm
       end
     end
