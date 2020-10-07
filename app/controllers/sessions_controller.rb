71f6ed2776ae417646323142a5af1478fe83efee
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-07 12:43:14 +0630

    login_out_contro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash.now[:danger] = 'Login Failed!'
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = 'ログアウトしました'
+    flash[:notice] = 'Logged out!'
     redirect_to new_session_path
   end
 end
