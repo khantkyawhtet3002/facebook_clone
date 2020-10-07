9846a26d4ea7edfc443c1d4780ba6b5372eed8e1
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-07 12:41:22 +0630

    image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
   process resize_to_fit: [250, 200]
   version :thumb do
   process resize_to_fit: [100, 100]
