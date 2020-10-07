890896dbda2282f26a1542793c8048bffdfb4686
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-07 12:46:05 +0630

    feed_contro

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,18 +1,13 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-  # GET /feeds
-  # GET /feeds.json
   def index
     @feeds = Feed.all
   end
-
-  # GET /feeds/1
-  # GET /feeds/1.json
+  
   def show
   end
 
-  # GET /feeds/new
   def new
     if params[:back]
       @feed = Feed.new(feed_params)
@@ -25,12 +20,9 @@ class FeedsController < ApplicationController
     @feed = Feed.new(feed_params)
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds
-  # POST /feeds.json
   def create
       @feed = Feed.new(feed_params)
       @feed.user_id = current_user.id
@@ -38,17 +30,17 @@ class FeedsController < ApplicationController
         render :new
       else
         if @feed.save
-          redirect_to feeds_path, notice: "Picture was successfully created."
+          redirect_to feeds_path, notice: "Post was successfully created."
         else
           render :new
         end
       end
     end
-     # PATCH/PUT /feeds/1.json
+
   def update
     respond_to do |format|
       if @feed.update(feed_params)
-        format.html { redirect_to @feed, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to @feed, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @feed }
       else
         format.html { render :edit }
@@ -57,12 +49,10 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
-      format.html { redirect_to feeds_url, notice: 'Feed was successfully destroyed.' }
+      format.html { redirect_to feeds_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
