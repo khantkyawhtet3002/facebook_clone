e2c2763fba77d73180d6da5b3e190072a55975f6
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-10-07 12:23:16 +0630

    create_fbclone_page

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,11 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  storage :file
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  process resize_to_fit: [250, 200]
+  version :thumb do
+  process resize_to_fit: [100, 100]
+  end
+end
